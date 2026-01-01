02c17d78b916df5f02a1553b95e7f4441f20c6d2
faizisyellow <114222238+faizisyellow@users.noreply.github.com>
2026-01-01 15:25:55 +0700

    feat: display description of the app if the users run without any CLI parameters

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import typer
 
-app = typer.Typer()
+app = typer.Typer(no_args_is_help=True)
 
 
-@app.command()
-def main():
-    print("Hello, World!!")
+@app.callback(invoke_without_command=True)
+def callback():
+    """
+    Plug Password
+
+    A tool for managing backup passwords securely using SQlite.
+    """
 
 
 if __name__ == "__main__":
