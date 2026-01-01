ea5c06f12852e6881d45a025efbd2b4804a75680
faizisyellow <114222238+faizisyellow@users.noreply.github.com>
2026-01-01 19:59:56 +0700

    update: replace callback for display description with help command

--- main.py
+++ main.py
@@ -1,17 +1,9 @@
 import typer
 
-app = typer.Typer(no_args_is_help=True)
-
-
-@app.callback(invoke_without_command=True)
-def callback():
-    """
-    Plug Password
-
-    A tool for managing backup passwords securely using SQLite.
-    """
-
-    raise typer.Exit()
+app = typer.Typer(
+    no_args_is_help=True,
+    help="Plug Password\n\n\nA tool for managing backup passwords securely with SQLite",
+)
 
 
 if __name__ == "__main__":
